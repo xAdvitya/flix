12c6c8df4daeadcb970257d9947c76126163eb2b
xAdvitya <advityasharma21@gmail.com>
2023-09-21 01:17:54 +0530

    banner

--- src/Banner.js
+++ src/Banner.js
@@ -1,27 +1,54 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Banner.css';
+import axios from 'axios';
+import requests from './Requests';
+import instance from './axios';
+
 function Banner() {
   function truncate(string, n) {
     return string?.length > n ? string.substr(0, n - 1) + '...' : string;
   }
+
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await instance.get(requests.fetchNetflixOriginals);
+      console.log(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
+      console.log(movie);
+      return request;
+    }
+    fetchData();
+  }, []);
   return (
     <header
       className="banner"
       style={{
         backgroundSize: 'cover',
-        backgroundImage: `url("https://i.imgur.com/e1hLQ2m.png")`,
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
         backgroundPosition: 'center center',
       }}
     >
       <div className="banner__contents">
-        <h1 className="banner_title">Movie Name</h1>
+        <h1 className="banner_title">
+          {movie?.name || movie?.title || movie?.original_name}
+        </h1>
 
         <div className="banner__buttons">
           <button className="banner__button">Play</button>
           <button className="banner__button">My List</button>
         </div>
         <h1 className="banner__description">
-          {truncate(`test descriptin`, 150)}
+          {truncate(movie?.overview, 150)}
         </h1>
       </div>
 
--- src/Requests.js
+++ src/Requests.js
@@ -1,6 +1,14 @@
-const API_KEY ="f4872214e631fc876cb43e6e30b7e731";
+const API_KEY = 'f4872214e631fc876cb43e6e30b7e731';
 
 const requests = {
-    fetchTrending:'/trending/all/week?api_key=${API_KEY}&language=en-US',
-    
-}
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv/?api_key=${API_KEY}&with_network=213`,
+  fetchTopRated: `/movies/top_rated?api_key${API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key${API_KEY}&with_genres=27`,
+  fetchRomanceyMovies: `/discover/movie?api_key${API_KEY}&with_genres=10749`,
+  fetchDocumentries: `/discover/movie?api_key${API_KEY}&with_genres=99`,
+};
+
+export default requests;
